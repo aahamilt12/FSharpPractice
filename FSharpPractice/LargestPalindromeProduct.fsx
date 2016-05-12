9ec69c3b7f22569f9f17980977d5b67e241e0ecf
Andrew Hamilton <aahamilt12@gmail.com>
2016-05-11 20:53:57 -0500

    LargestPalindromeProduct initial commit

---
+++ FSharpPractice/LargestPalindromeProduct.fsx
@@ -0,0 +1,7 @@
+﻿// Practice Problem 4 https://projecteuler.net/problem=4
+
+let ReverseString (s: string) =
+     (System.String (Array.rev (s.ToCharArray()))).ToString()
+let IsPalindrome num = ReverseString(num.ToString()) = num.ToString()
+
+IsPalindrome 1111
